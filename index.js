95dfc74253bc83b96ed8d18e34cf0e8ab0b35c1a
anayet ullah <anayetdev@gmail.com>
2023-05-04 09:30:08 +0600

    Server app created and it is  working

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express();
 const port = 5000;
-
+const allData = require('./data.json');
 app.get('/', (req, res) => {
   res.send('Server is working!');
+})
+
+app.get('/data', (req, res) => {
+    res.send(allData);
 })
 
 app.listen(port, () => {
