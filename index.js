3d9f290adbe26a78a2184647ef16d18cee02c1e7
anayet ullah <anayetdev@gmail.com>
2023-05-04 12:38:43 +0600

    Single data functionality added for each chef

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+app.use(cors())
+
 const allData = require('./data.json');
+
 app.get('/', (req, res) => {
   res.send('Server is working!');
 })
 
-app.get('/data', (req, res) => {
+app.get('/chefs', (req, res) => {
     res.send(allData);
 })
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = allData.find(data => data.id == id);
+    res.send(selectedChef);
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
